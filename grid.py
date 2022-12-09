3faac7b7aadcedb1dc1e0f6a65ed5756e91fc69d
supermuesli <>
2022-12-09 18:06:51 +0100

    verify grid.json settings before loading

--- grid.py
+++ grid.py
@@ -27,7 +27,7 @@ class Grid:
         # distribution keeps track of which component ID relies on which other component ID
         self.distribution = {}
 
-        # load gridData
+        # verify and load gridData
         for key in gridData:
             if key == 'cellSize':
                 self.cellSize = gridData[key]
@@ -37,73 +37,96 @@ class Grid:
                 for gc in gcs:
                     for x in range(round(gc[0][0]), round(gc[1][0])+1):
                         for y in range(round(gc[0][1]), round(gc[1][1])+1):
+                            if x >= self.gridSize or y >= self.gridSize:
+                                print('could not add cell (%d, %d) as cell overflows the grid' % (x, y))
+                                continue
+        
                             self.cells[x][y] = True
 
             if key == 'providers':
                 ps = gridData[key]
                 for p in ps:
-                    if not self.occupiedCells[p['coordX']][p['coordY']]:
-                        self.providers.append(
-                            Provider(
-                                id=p['id'],
-                                coordX=p['coordX'],
-                                coordY=p['coordY'],
-                                desiredKWH=p['desiredKWH']
-                            )
+                    if p['coordX'] >= self.gridSize or p['coordY'] >= self.gridSize:
+                        print('could not add provider "%s" as cell coordinates overflow the grid' % p['displayName'])
+                        continue
+
+                    if self.occupiedCells[p['coordX']][p['coordY']]:
+                        print('could not add provider "%s" as cell was already occupied' % p['displayName'])
+                        continue
+
+                    self.providers.append(
+                        Provider(
+                            id=p['id'],
+                            coordX=p['coordX'],
+                            coordY=p['coordY'],
+                            desiredKWH=p['desiredKWH']
                         )
-                        self.occupiedCells[p['coordX']][p['coordY']] = True
-                    else:
-                        print('could not add provider %s as cell was already occupied' % p['displayName'])
+                    )
+                    self.occupiedCells[p['coordX']][p['coordY']] = True
 
             if key == 'users':
                 us = gridData[key]
                 for u in us:
-                    if not self.occupiedCells[u['coordX']][u['coordY']]:
-                        self.users.append(
-                            User(
-                                id=u['id'],
-                                coordX=u['coordX'],
-                                coordY=u['coordY'],
-                                desiredKWH=u['desiredKWH']
-                            )
-                        )
-                        self.occupiedCells[u['coordX']][u['coordY']] = True
-                    else:
-                        print('could not add user %s as cell was already occupied' % u['displayName'])
+                    if u['coordX'] >= self.gridSize or u['coordY'] >= self.gridSize:
+                        print('could not add user "%s" as cell coordinates overflow the grid' % u['displayName'])
+                        continue
 
+                    if self.occupiedCells[u['coordX']][u['coordY']]:
+                        print('could not add user %s as cell was already occupied' % u['displayName'])
+                        continue
+
+                    self.users.append(
+                        User(
+                            id=u['id'],
+                            coordX=u['coordX'],
+                            coordY=u['coordY'],
+                            desiredKWH=u['desiredKWH']
+                        )
+                    )
+                    self.occupiedCells[u['coordX']][u['coordY']] = True
 
             if key == 'stores':
                 ss = gridData[key]
                 for s in ss:
-                    if not self.occupiedCells[s['coordX']][s['coordY']]:
-                        self.stores.append(
-                            Store(
-                                id=s['id'],
-                                coordX=s['coordX'],
-                                coordY=s['coordY'],
-                                desiredKWH=s['desiredKWH']
-                            )
-                        )
-                        self.occupiedCells[s['coordX']][s['coordY']] = True
-                    else:
-                        print('could not add store %s as cell was already occupied' % s['displayName'])
+                    if s['coordX'] >= self.gridSize or s['coordY'] >= self.gridSize:
+                        print('could not add store "%s" as cell coordinates overflow the grid' % s['displayName'])
+                        continue
 
+                    if self.occupiedCells[s['coordX']][s['coordY']]:    
+                        print('could not add store %s as cell was already occupied' % s['displayName'])
+                        continue
+
+                    self.stores.append(
+                        Store(
+                            id=s['id'],
+                            coordX=s['coordX'],
+                            coordY=s['coordY'],
+                            desiredKWH=s['desiredKWH']
+                        )
+                    )
+                    self.occupiedCells[s['coordX']][s['coordY']] = True
+                        
             if key == 'p2xs':
                 ps = gridData[key]
                 for p in ps:
-                    if not self.occupiedCells[p['coordX']][p['coordY']]:
-                        self.p2xs.append(
-                            Store(
-                                id=p['id'],
-                                coordX=p['coordX'],
-                                coordY=p['coordY'],
-                                desiredKWH=p['desiredKWH']
-                            )
-                        )
-                        self.occupiedCells[p['coordX']][p['coordY']] = True
-                    else:
-                        print('could not add p2x %s as cell was already occupied' % p['displayName'])
+                    if p['coordX'] >= self.gridSize or p['coordY'] >= self.gridSize:
+                        print('could not add p2x "%s" as cell coordinates overflow the grid' % p['displayName'])
+                        continue
 
+                    if self.occupiedCells[p['coordX']][p['coordY']]:    
+                        print('could not add p2x %s as cell was already occupied' % p['displayName'])
+                        continue
+
+                    self.p2xs.append(
+                        Store(
+                            id=p['id'],
+                            coordX=p['coordX'],
+                            coordY=p['coordY'],
+                            desiredKWH=p['desiredKWH']
+                        )
+                    )
+                    self.occupiedCells[p['coordX']][p['coordY']] = True
+                    
     def step(self):
         # compute optimal energy flow ->
         # map each component to the component it is going to use in the next timestep
